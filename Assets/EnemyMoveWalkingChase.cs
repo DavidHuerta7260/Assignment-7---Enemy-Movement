20322790b677d9532da2798690909e8457f76cac
DavidHuerta <dhuer7260@365.elmhurst.edu>
2025-05-23 22:53:27 -0500

    Assignment7

---
+++ Assets/EnemyMoveWalkingChase.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Profiling;
+using UnityEngine;
+
+public class EnemyMoveWalkingChase : MonoBehaviour
+{
+    public float chaseRange = 4f;
+    public float enemyMovementSpeed = 1.5f;
+
+    private Transform playerTransform;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>(); // ✅ Correct assignment
+        playerTransform = GameObject.FindWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        Vector2 playerDirection = playerTransform.position - transform.position;
+        float distanceToPlayer = playerDirection.magnitude;
+
+        if (distanceToPlayer <= chaseRange)
+        {
+            playerDirection.Normalize();
+            playerDirection.y = 0f;
+
+            FacePlayer(playerDirection);
+
+            if (IsGroundedAhead())
+            {
+                MoveTowardsPlayer(playerDirection);
+            }
+            else
+            {
+                StopMoving();
+                Debug.Log("No ground ahead");
+            }
+        }
+        else
+        {
+            StopMoving();
+        }
+    }
+
+    private bool IsGroundedAhead()
+    {
+        float groundCheckDistance = 2.0f;
+        LayerMask groundLayer = LayerMask.GetMask("Ground");
+
+        Vector2 direction = Vector2.down;
+        float offsetX = playerTransform.position.x < transform.position.x ? -0.5f : 0.5f;
+        Vector2 origin = new Vector2(transform.position.x + offsetX, transform.position.y);
+
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, groundCheckDistance, groundLayer);
+        Debug.DrawRay(origin, direction * groundCheckDistance, Color.red); // Optional for debugging
+
+        return hit.collider != null;
+    }
+
+    private void FacePlayer(Vector2 playerDirection)
+    {
+        if (playerDirection.x < 0)
+        {
+            sr.flipX = false; // Faces left
+        }
+        else
+        {
+            sr.flipX = true;  // Faces right
+        }
+    }
+
+
+    private void MoveTowardsPlayer(Vector2 playerDirection)
+    {
+        rb.velocity = new Vector2(playerDirection.x * enemyMovementSpeed, rb.velocity.y);
+        anim.SetBool("isMoving", true);
+    }
+
+    private void StopMoving()
+    {
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        anim.SetBool("isMoving", false);
+    }
+}
---
+++ Assets/Scripts/PlatformerPlayerController.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayerController : MonoBehaviour
+{
+    // Player movement speed
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f; // Make jumpForce public or use SerializeField
+
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+
+    private Rigidbody2D rb;
+    public bool isGrounded;
+    private float horizontalInput;
+
+    public AudioClip jumpSound;
+
+    private AudioSource playerAudio;
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        playerAudio = GetComponent<AudioSource>();
+
+        animator = GetComponent<Animator>();
+
+        if (groundCheck == null)
+        {
+            Debug.LogError("GroundCheck not assigned to the player controller!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            if (playerAudio != null && jumpSound != null)
+            {
+                playerAudio.PlayOneShot(jumpSound, 1.0f);
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        animator.SetFloat("xVelocityAbs", Mathf.Abs(rb.velocity.x));
+
+        animator.SetFloat("yVelocity", rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        animator.SetBool("onGround", isGrounded);
+
+        if (horizontalInput > 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if(!PlayerHealth.hitRecently)
+        {
+            rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+        }
+    }
+}
+
